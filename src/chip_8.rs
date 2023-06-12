6ef01c91a1cc6e798286d4372fc6e9fbb5d70d63
Connor Luebbehusen <connorluebbehusen@gmail.com>
2023-06-12 18:08:25 -0400

    add basic instructions

--- src/chip_8.rs
+++ src/chip_8.rs
@@ -1,4 +1,22 @@
+use std::time;
+
 use crate::constants;
+
+fn get_epoch_ns() -> u128 {
+    time::SystemTime::now()
+        .duration_since(time::UNIX_EPOCH)
+        .unwrap()
+        .as_nanos()
+}
+
+struct ParsedInstruction {
+    opcode: u8,
+    x: u8,
+    y: u8,
+    n: u8,
+    nn: u8,
+    nnn: u16,
+}
 
 pub struct Chip8 {
     ram: [u8; constants::RAM_LEN],
@@ -13,6 +31,8 @@ pub struct Chip8 {
 
     debug: bool,
     instruction_time: u128,
+    start_time: u128,
+    update_display: bool,
 }
 
 impl Chip8 {
@@ -24,6 +44,8 @@ impl Chip8 {
         ram[constants::FONT_START..constants::FONT_END].copy_from_slice(&constants::FONT);
         let program_end = constants::PROGRAM_START + bytes.len();
         ram[constants::PROGRAM_START..program_end].copy_from_slice(&bytes);
+
+        let start_time = get_epoch_ns();
 
         Chip8 {
             ram,
@@ -37,7 +59,107 @@ impl Chip8 {
             display: [false; constants::DISPLAY_LEN],
 
             debug,
+            start_time,
             instruction_time,
+            update_display: false,
         }
+    }
+
+    fn fetch_instruction(&mut self) -> u16 {
+        let instruction_first_byte = self.ram[self.program_counter];
+        let instruction_second_byte = self.ram[self.program_counter + 1];
+        self.program_counter += 2;
+
+        ((instruction_first_byte as u16) << 8) | instruction_second_byte as u16
+    }
+
+    fn parse_instruction(instruction: u16) -> ParsedInstruction {
+        ParsedInstruction {
+            opcode: ((instruction & 0xF000) >> 8) as u8,
+            x: ((instruction & 0x0F00) >> 8) as u8,
+            y: ((instruction & 0x00F0) >> 4) as u8,
+            n: (instruction & 0x000F) as u8,
+            nn: (instruction & 0x00FF) as u8,
+            nnn: instruction & 0x0FFF,
+        }
+    }
+
+    pub fn cycle(&mut self) {
+        let instruction = self.fetch_instruction();
+        let parsed_instruction = Chip8::parse_instruction(instruction);
+
+        if self.debug {
+            println!(
+                "Instruction: {:04X} | Opcode: {:X} | X: {:X} | Y: {:X} | N: {:X} | NN: {:X} | NNN: {:X}",
+                instruction,
+                parsed_instruction.opcode,
+                parsed_instruction.x,
+                parsed_instruction.y,
+                parsed_instruction.n,
+                parsed_instruction.nn,
+                parsed_instruction.nnn,
+            );
+        }
+
+        match parsed_instruction.opcode {
+            0x00 => match parsed_instruction.nn {
+                0xE0 => self.clear_screen(),
+                0xEE => self.return_from_subroutine(),
+                _ => panic!(
+                    "Unrecognized second byte: {:X} for opcode: {:X}",
+                    parsed_instruction.nn, parsed_instruction.opcode
+                ),
+            },
+            0x10 => self.jump(parsed_instruction.nnn),
+            0x60 => self.set_register(parsed_instruction.x, parsed_instruction.nn),
+            0x70 => self.add_to_register(parsed_instruction.x, parsed_instruction.nn),
+            0xA0 => self.set_index_register(parsed_instruction.nnn),
+            0xD0 => self.display(
+                parsed_instruction.x,
+                parsed_instruction.y,
+                parsed_instruction.n,
+            ),
+            _ => panic!("Unrecognized opcode: {:X}", parsed_instruction.opcode),
+        }
+    }
+
+    // 0x00E0
+    fn clear_screen(&mut self) {
+        self.display = [false; constants::DISPLAY_LEN];
+        self.update_display = false;
+    }
+
+    // 0x00EE
+    fn return_from_subroutine(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack pointer is 0, cannot return from subroutine");
+        }
+        self.program_counter = self.stack[self.stack_pointer as usize] as usize;
+        self.stack_pointer -= 1;
+    }
+
+    // 0x1NNN
+    fn jump(&mut self, address: u16) {
+        self.program_counter = address as usize;
+    }
+
+    // 0x6XNN
+    fn set_register(&mut self, register: u8, value: u8) {
+        self.registers[register as usize] = value;
+    }
+
+    // 0x7NN
+    fn add_to_register(&mut self, register: u8, value: u8) {
+        self.registers[register as usize] = self.registers[register as usize].wrapping_add(value);
+    }
+
+    // 0xANNN
+    fn set_index_register(&mut self, value: u16) {
+        self.index_register = value;
+    }
+
+    // 0xDXYN
+    fn display(&mut self, x_register: u8, y_register: u8, height: u8) {
+        todo!("Implement display")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ mod constants;
 use chip_8::Chip8;
 
 fn main() {
-    let _chip8 = Chip8::build("", 1000, false);
+    let mut chip8 = Chip8::build("", 1000, false);
+    chip8.cycle();
 }
